ae3a3aeddcd9bec52980dec563a8e3db50b36800
Denperidge <denperidge@gmail.com>
2023-04-06 12:51:27 +0200

    Fxed prism crash, WIP default select

--- app/controllers/docs/doc.js
+++ app/controllers/docs/doc.js
@@ -3,12 +3,34 @@ import { tracked } from 'tracked-built-ins';
 import { action } from '@ember/object';
 
 export default class DocsDocController extends Controller {
-    @tracked revision = null;
-    @tracked readme = null;
+    queryParams = ["revision"]
+    @tracked revision;
+
+
+    init() {
+        console.log(this.model)
+        super.init(...arguments)
+        console.log(this.model)
+
+        if (this.revision == null) {
+            console.log("No revision selected")
+            console.log(this)
+        }
+
+        try {
+            console.log(this.revision)
+        } catch {
+            console.log("No revision selected")
+            console.log(this)
+        }
+ 
+    }
 
     @action
     selectRevision(revision) {
-        this.revision = revision;
+-        this.target.currentState.router.transitionTo({
+            queryParams: {revision: revision}
+        })
     }
 
 }
--- app/routes/docs/doc.js
+++ app/routes/docs/doc.js
@@ -1,12 +1,8 @@
 import Route from '@ember/routing/route';
-import { tracked } from 'tracked-built-ins';
-import { action } from '@ember/object';
 
 export default class DocsDocRoute extends Route {
-
     model(params) {
         let repoName = params.name;
-        let revision = this.revision;        
 
         return this.store.query("repo", {
             filter: {
@@ -14,14 +10,7 @@ export default class DocsDocRoute extends Route {
             },
             include: "revisions"
         }).then(function(repos) {
-            let repo = repos.get("firstObject");
-            
-            return { 
-                repo: repo,
-                revisions: repo.revisions.map((revision) => revision.repoTag)
-            };
+            return repos.get("firstObject");            
         })
-        
-        //return `<zero-md src='${repo.contentBaseUrl}README.md'></zero-md>`;
     }
 }
