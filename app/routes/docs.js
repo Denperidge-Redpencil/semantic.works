56901a208b0a3a086da5967d6a7ba0078709acda
Denperidge <denperidge@gmail.com>
2023-04-06 06:33:29 +0200

    First succesful data import
    - Set proxy in npm start
    - Fixed serializer deprecation warning
    - Replaced ember-fetch with node-fetch

--- app/routes/docs.js
+++ app/routes/docs.js
@@ -1,4 +1,9 @@
 import Route from '@ember/routing/route';
 
 export default class DocsRoute extends Route {
+    model() {
+        this.store.findAll("repo").then(function (repos) {
+            console.log(repos)
+        })
+    }
 }
