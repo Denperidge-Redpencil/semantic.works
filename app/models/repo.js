3214be41adaa0f10d8e416751cedcb75cbdbdd54
Denperidge <denperidge@gmail.com>
2023-04-06 07:16:12 +0200

    Repos now loaded into docs nav

--- app/models/repo.js
+++ app/models/repo.js
@@ -1,5 +1,8 @@
-import Model from '@ember-data/model';
+import Model, {attr} from '@ember-data/model';
 
 export default class RepoModel extends Model {
-
+    @attr title;
+    @attr description;
+    @attr category;
+    @attr imageUrl;
 }
--- app/routes/docs.js
+++ app/routes/docs.js
@@ -1,9 +1,30 @@
 import Route from '@ember/routing/route';
 
 export default class DocsRoute extends Route {
-    model() {
-        this.store.findAll("repo").then(function (repos) {
-            console.log(repos)
-        })
+    async model() {
+        let repos = await this.store.findAll("repo");
+        let categories = [];
+        repos.forEach(repo => {
+            let repoCategory = repo.category.substring(repo.category.lastIndexOf("/") + 1);
+
+            let category = categories.find(category => category.name == repoCategory);
+            if (category == undefined) {
+                categories.push({
+                    name: repoCategory,
+                    repos: [repo]
+                })
+            } else {
+                category.repos.push(repo);
+            }
+        });
+
+        return categories;
+
+
+
+        /*
+        return this.store.findAll("repo", { include: "repo-revisions"}).map((val) => 
+            val.title
+        );*/
     }
 }
