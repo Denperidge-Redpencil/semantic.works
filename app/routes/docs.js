4b3e322b8b5203dc32af0f3cd5ff437dd5aee7ca
Denperidge <denperidge@gmail.com>
2023-04-07 16:15:46 +0200

    Categories now sorted, excluded archive

--- app/routes/docs.js
+++ app/routes/docs.js
@@ -1,11 +1,27 @@
 import Route from '@ember/routing/route';
 
+const CATEGORY_WEIGHTS = {
+    core: 0,
+    templates: 1,
+    microservices: 2,
+    "ember-addons": 3,
+    tools: 4
+};
+
+const CATEGORY_EXCLUSIONS = [
+    "archive"
+];
+
 export default class DocsRoute extends Route {
     async model() {
         let repos = await this.store.findAll("repo");
         let categories = [];
         repos.forEach(repo => {
             let repoCategory = repo.category.substring(repo.category.lastIndexOf("/") + 1);
+
+            if (CATEGORY_EXCLUSIONS.indexOf(repoCategory) > -1) {
+                return
+            }
 
             let category = categories.find(category => category.name == repoCategory);
             if (category == undefined) {
@@ -21,6 +37,10 @@ export default class DocsRoute extends Route {
                 });
             }
         });
+
+        categories.sort(function(a, b) {
+            return CATEGORY_WEIGHTS[a.name] > CATEGORY_WEIGHTS[b.name]
+        })
 
 
         return categories;
