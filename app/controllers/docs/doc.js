a9913fc4ea8fbbfd275820b219667e2b063f2f44
Denperidge <denperidge@gmail.com>
2023-04-07 13:40:46 +0200

    Docs load on page open, based on queryParam

--- app/controllers/docs/doc.js
+++ app/controllers/docs/doc.js
@@ -3,34 +3,15 @@ import { tracked } from 'tracked-built-ins';
 import { action } from '@ember/object';
 
 export default class DocsDocController extends Controller {
-    queryParams = ["revision"]
-    @tracked revision;
+    queryParams = ["revision"];
 
-
-    init() {
-        console.log(this.model)
-        super.init(...arguments)
-        console.log(this.model)
-
-        if (this.revision == null) {
-            console.log("No revision selected")
-            console.log(this)
-        }
-
-        try {
-            console.log(this.revision)
-        } catch {
-            console.log("No revision selected")
-            console.log(this)
-        }
- 
-    }
-
+    
     @action
     selectRevision(revision) {
 -        this.target.currentState.router.transitionTo({
             queryParams: {revision: revision}
         })
     }
+    
 
 }
--- app/routes/docs/doc.js
+++ app/routes/docs/doc.js
@@ -1,7 +1,9 @@
 import Route from '@ember/routing/route';
+import { tracked } from 'tracked-built-ins';
 
 export default class DocsDocRoute extends Route {
     model(params) {
+        console.log("ee")
         let repoName = params.name;
 
         return this.store.query("repo", {
@@ -12,5 +14,15 @@ export default class DocsDocRoute extends Route {
         }).then(function(repos) {
             return repos.get("firstObject");            
         })
+    }
+
+    async setupController(controller, model, transition) {
+        super.setupController(controller, model, transition)
+
+        let revisionParam = transition.to.queryParams.revision;
+        let revisions = await model.revisions;
+        let revisionObject = revisions.find((revision) => revision.repoTag == revisionParam); //controller.revision);
+
+        controller.revision = revisionObject;
     }
 }
