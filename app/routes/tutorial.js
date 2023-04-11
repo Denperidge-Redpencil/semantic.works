2c25a9ad58055ee6a098ec395fa5365567d4a6a6
Denperidge <denperidge@gmail.com>
2023-04-11 09:57:11 +0200

    Implemented /tutorial

--- app/routes/tutorial.js
+++ app/routes/tutorial.js
@@ -1,4 +1,19 @@
 import Route from '@ember/routing/route';
 
 export default class TutorialRoute extends Route {
+    model() {
+        return this.store.query("repo", {
+            filter: {
+                "title": "mu-project"
+            },
+            include: "revisions"
+        }).then(function(repos) {
+            let repo = repos.get("firstObject");        
+            return repo.revisions.then((revisions) => {
+                let readme = revisions.get("firstObject").readme;
+                let tutorials = new RegExp("Tutorials((.|\n)*)", "gmi").exec(readme)
+                return tutorials
+            });
+        });
+    }
 }
